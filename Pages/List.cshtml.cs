8bca2c160807344bc8364b79787dba02cc769611
Chandramurthy <195500@cognizant.com>
2019-07-19 19:37:54 +0530

    new page for browse dictionary

---
+++ Pages/List.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NewDictionary.Entity;
+using NewDictionary.Interfaces;
+using MongoDB.Driver;
+using NewDictionary.Models.KnownValues;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace NewDictionary.Pages
+{
+    public class ListModel : PageModel
+    {
+        private readonly IDictionaryRepository _dictionaryRepository;
+
+        [BindProperty]
+        public List<Word> Words {get;set;}
+        [BindProperty(SupportsGet = true)]
+        public string SearchKey{get;set;}
+
+        public ListModel(IDictionaryRepository dictionaryRepository){
+            _dictionaryRepository = dictionaryRepository;
+        }
+
+        public IActionResult OnGet(){
+            Words = new List<Word>();
+            if(!string.IsNullOrEmpty(SearchKey)){
+                var result = _dictionaryRepository.GetWordsByField(SearchField.English, SearchKey, SearchType.StartsWith, null).Result;
+                Words.AddRange(result);
+            }
+            else{
+                 Words.AddRange(_dictionaryRepository.GetAllWords().Result);
+            }
+            return Page();
+        }
+    }
+}
